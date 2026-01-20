ff41205f977be64a15d1f61fbcb901f7455d9b92
Evan Lambert <elambert@veygax.dev>
2026-01-20 22:05:58 +0000

    feat: add basic portal prompt
    Signed-off-by: Evan Lambert <elambert@veygax.dev>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use ashpd::desktop::remote_desktop::{DeviceType, RemoteDesktop};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let portal = RemoteDesktop::new().await?;
+    let session = portal.create_session().await?;
+
+    portal
+        .select_devices(
+            &session,
+            DeviceType::Keyboard | DeviceType::Pointer,
+            None,
+            ashpd::desktop::PersistMode::Application,
+        )
+        .await?;
+
+    println!("Waiting for Portal prompt");
+
+    portal.start(&session, None).await?;
+
+    println!("Got tracking!");
+
+    Ok(())
 }
